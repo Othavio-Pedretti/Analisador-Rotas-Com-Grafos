25c9b9ac220646375c82543194dfcf058c866a68
arthur <209285@upf.br>
2026-05-27 17:02:49 -0300

    logica de separação dos dado

--- graph.cpp
+++ graph.cpp
@@ -1,58 +1,158 @@
-//biblioteca para grafos
+/// biblioteca para grafos
 #include <string>
 #include <unordered_map>
 #include <vector>
 #include <fstream>
 #include <cstdlib>
+#include <algorithm>
+#include <unordered_set>
 
 namespace graph{
 
     class digraph{
         private:
-            // nodos do grafo - VERTICES
-            struct node{
-                std::string value; //valor
-                std::vector<node*> links; //vizinhos: lista de ponteiros para os nodos vizinhos -ARESTAS --NESTE CASO IRA APENAS O HOP_TO
-            }; 
+          // nodos do grafo - VERTÍCES
+          struct node{
+            std::string prb_id;
+            std::string probe_src;
+            std::string dst_addr;
+            float rtt;
+            std::vector<node*> links; //lista de ponteiros p/ os nodos vizinhos - ARESTAS
+          };
 
-        //tabela hash, que mapeia o rotulo do no para o objeto node
-                            //<chave>, <valor>
-        std::unordered_map<std::string, node> nodes;
-
-        public:
-            //insere um novo nodo no grafo com o rotulo s
-            void insert_node(const std::string& s){
-                node aux;
-                aux.value = s;
-                nodes[s] = aux;
-            }
+          //tabela hash, que mapeia o rótulo do nó para o objeto node
+                             //<chave>, <valor>    
+          std::unordered_map<std::string,node> nodes;
+          std::unordered_set<node*> visited;
         
-            //retorna a quantidade de nodos do grafo
-         size_t size(){
+        public:
+         //Insere um novo nó no grafo com o rótulo s
+          void insert_nodo(const std::string& s){
+            node aux;
+            aux.value = s;
+            nodes[s] = aux;
+          }
+
+          //retorna a quantidade de nós (vertices) do grafo
+          size_t size(){
             return nodes.size();
-         }
+          }
 
-        //busca um node pelo seu rotulo e retorna o endereço do nodo
-        node* find(const std::string& s){
-            auto it = nodes.find(s);
-            return it == nodes.end() ? nullptr : &it->second;
-        }
+          //busca um nó pelo seu rótulo e retorna o endereço do nodo 
+          node* find(const std::string& s){
+            auto it = nodes.find(s);//este find é do unordere_map
+            return it==nodes.end() ?  nullptr : &it->second;
+          }
 
-        //insere um aresta de 'from' para 'to'
-        bool insert_link(const std::string& from, const std::string& to){
-            auto pfrom = find(from);
-            if(pfrom == nullptr) return false; //nodo de origem nao existe
-            auto pto = find(to);
-            if(pto == nullptr) return false; //nodo de destino nao existe
+          //INsere um aresta dirigida de 'from' para 'to'
+          bool insert_link(const std::string& hop_from, const std::string& hop_to){
+            auto pfrom = find(hop_from);
+            if(pfrom == nullptr) return false; // nó de origem ñ existe
+            auto pto = find(hop_to);
+            if(pto == nullptr) return false; // nó de destino ñ existe
 
-            pfrom->links.push_back(pto);//adiciona a aresta na lista
+            pfrom->links.push_back(pto); //Adiciona a aresta na lista de adjacencia
             return true;
-        }
-    };
+          }
 
-}  //fim do namespace
+          //numero de arestas que saem de um vertice
+          size_t outdegree(const std::string& s){
+            for(auto node : nodes){
+              if(node.first == s){
+                return node.second.links.size();
+              }
+            }
+            return 0;
+          }
 
+          //numero de arestas que chegam a um vertice
+          size_t indegree(const std::string& s){
+            auto p = find(s);
+            if(p == nullptr){
+              return 0;
+            }
+            size_t qnt = 0;
+            for(auto vertice : nodes){
+                for(auto link : vertice.second.links)
+                  if(link->value == s){
+                    qnt++;
+                  }
+            }
+            return qnt;
+          }
 
+          //numero de arestas conectadas a um vertice
+          size_t degree(const std::string& s){
+            auto p = find(s);
+            if(p == nullptr){
+              return 0;
+            }
+            return indegree(s) + outdegree(s);
+          }
 
+          void export2dot(const std::string& filename){
+            std::ofstream dot(filename); // cria o arquivo
+            dot << "digraph {\n";
+            
+            for(auto nd : nodes){ //  percorre todos os nodos
+              dot << "\t" << nd.first; //escreve o rotulo do nodo (chave) 
+              if(nd.second.links.size() > 0){
+                dot << " -> {";
+                for(auto vizinho : nd.second.links){
+                  dot << vizinho->value << " ";
+                }
+                dot << "}"; //fecha chave da lista dos vizinhos
+              }
+              dot << ";\n"; 
+            }
+            dot << "}\n";//fecha chaves do digraph
+            
+          }
+          
+          void draw(){
+            export2dot("graphED2.dot");
+            std::system("dot -Tx11 graphED2.dot");
 
+          }
 
+          void remove_link(const std::string &from, const std::string &to){
+            auto pfrom = find(from);
+            if(!pfrom) return;
+            auto pto = find(to);
+            if(!pto) return;   
+            
+            auto it = std::find(pfrom->links.begin(), pfrom->links.end(), pto);
+            if(it == pfrom->links.end()) return;
+            pfrom->links.erase(it);
+          }
+
+          void remove_nodo(const std::string &key){
+            //encontrar se aquele nodo é vizinho de algum outro nodo e remover
+            auto p = find(key);
+            if(!p) return;
+            for(auto &[k,nd] : nodes){
+              auto it = std::find(nd.links.begin(), nd.links.end(), p);
+              if(it != nd.links.end()) nd.links.erase(it);
+            }
+            nodes.erase(key);
+          }
+
+          void recursive_DFS(node* p, int level = 0){
+            if(visited.count(p) > 0)//ja foi visitado
+            return;
+            std::cout << std::string(level, '\t') << p->value << std::endl;
+            visited.insert(p);
+            for(auto ln : p->links){
+              recursive_DFS(ln, level+1);
+            }
+          } 
+
+          void DFS_from(const std::string &s){
+            auto p = find(s);
+            if(!p) return;
+            visited.clear();
+            recursive_DFS(p);
+          }
+    }; /// fim da classe digraph
+
+} //fim do namespace
--- program.cpp
+++ program.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <fstream>
-
+#include <sstream>
+//#include "graph.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-    string s;
+    string prb_id;
+    string probe_src;
+    string dst_addr;
+    float rtt;
+    string hop_from;
+    string hop_to;
+    string hop;
     ifstream arquivo_entrada;
     string entrada;
     for (int i = 1; i < argc; ++i) {
@@ -19,11 +26,15 @@ int main(int argc, char *argv[]){
         exit(1);
    }
 
-   /*sanitização*/
-
-   while (arquivo_entrada >> s)
+   string linha;
+   string lixo;
+   while (getline(arquivo_entrada, linha))
    {
-        cout << s << endl;
+        stringstream ss(linha);
+        if(getline(ss, prb_id, ',') && getline(ss, probe_src, ',')){
+            cout << prb_id << "\n"
+                << probe_src << "\n";
+        }
    }
    
     arquivo_entrada.close();
