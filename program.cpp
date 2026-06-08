a84180f41a0985a951b348c41d8513c9d02fcdf5
arthur <209285@upf.br>
2026-06-08 17:00:55 -0300

    menu

--- program.cpp
+++ program.cpp
@@ -12,6 +12,8 @@ int main(int argc, char *argv[]){
     string entrada;
     graph::digraph g;
     unordered_map<string, graph::digraph> map;
+    int arestas = 0;
+    int vertices = 0;
     for (int i = 1; i < argc; ++i) {
         entrada += argv[i];
     }
@@ -39,8 +41,10 @@ int main(int argc, char *argv[]){
              
             if(hop_to != "*"){
                 if(hop_from != " " || hop_to != " "){
-                    map[prb_id].nodes[hop] = {probe_src, dst_addr, rtt, hop_from};
-                    map[prb_id].nodes[hop].links.push_back(hop_to);
+                    map[prb_id].nodes[hop_from] = {hop, probe_src, dst_addr, rtt};
+                    map[prb_id].nodes[hop_from].links.push_back(hop_to);
+                    vertices++;
+                    arestas++;
                 }
             }
         }
@@ -49,12 +53,12 @@ int main(int argc, char *argv[]){
 
    for (auto& [prb_id, digrafo] : map) {
     std::cout << "prb_id: " << prb_id << "\n";
-        for (const auto& [hop, node] : digrafo.nodes) {
-            std::cout << "  hop: " << hop << "\n";
+        for (const auto& [hop_from, node] : digrafo.nodes) {
+            std::cout << "  hop_from: " << hop_from << "\n";
+            std::cout << "    hop: " << node.hop << "\n";
             std::cout << "    probe_src: " << node.probe_src << "\n";
             std::cout << "    dst_addr: "  << node.dst_addr  << "\n";
             std::cout << "    rtt: "       << node.rtt       << "\n";
-            std::cout << "    hop_from: "  << node.hop_from  << "\n";
             std::cout << "    links:";
             for (auto& destino : node.links) {
                 std::cout << " " << destino;
@@ -62,8 +66,24 @@ int main(int argc, char *argv[]){
             std::cout << "\n";
         }
     }
-    cout << g.size() << " nodos" << endl;
-    cout << g.find("192.168.3.1");
+    cout << "Carregamento inicial concluído! Foram inseridos " << vertices << " vertices e " << arestas << " arestas" << endl;
     arquivo_entrada.close();
+    cout << "======MENU======" << endl <<
+    "Digite a opção desejada:" << endl << 
+    "1: Exibir Grafo" << endl <<
+    "2: Encontrar o Menor Caminho" << endl <<
+    "3: Calcular o Diâmetro do Grafo" << endl <<
+    "4: Identificar Roteadores Críticos" << endl <<
+    "5: Sair" << endl <<
+    "================" << endl <<
+    "Escolha uma das opções acima: ";
+    int usuario;
+    cin >> usuario;
+
+    if(usuario == 1){
+        
+    }
+    
+    
     return 0;
 }
